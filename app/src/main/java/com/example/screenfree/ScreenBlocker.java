fab9ab551eb4995314f4725bcca68ac2748bca30
Diyar Faraj <diyar.faraj@gmail.com>
2021-05-11 19:44:10 +0200

    chrome bug

--- app/src/main/java/com/example/screenfree/ScreenBlocker.java
+++ app/src/main/java/com/example/screenfree/ScreenBlocker.java
@@ -1,11 +1,13 @@
 package com.example.screenfree;
 
+import android.app.ActivityManager;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.text.InputType;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.widget.EditText;
@@ -15,6 +17,8 @@ import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.List;
 
 public class ScreenBlocker extends AppCompatActivity {
 
@@ -58,5 +62,15 @@ public class ScreenBlocker extends AppCompatActivity {
         mDialog.show();
     }
 
+    @Override
+    protected void onPause() {
+        String activityOnTop;
+        ActivityManager mActivityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        List<ActivityManager.RunningTaskInfo> RunningTask = mActivityManager.getRunningTasks(1);
+        ActivityManager.RunningTaskInfo ar = RunningTask.get(0);
+         activityOnTop = ar.topActivity.getClassName();
 
+        Log.d("ON PAUUSSE: ","onPause:-------------------------- "+   activityOnTop);
+        super.onPause();
+    }
 }
